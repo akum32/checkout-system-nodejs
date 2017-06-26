321e00acf8d1ff81b825e4499f033677deb99e40
Asheesh Karwal <asheesh.karwal@myob.com>
2017-06-27 00:22:55 +1000

    checkout calculates total as sum of products when no price rules given

--- src/lib/checkout.js
+++ src/lib/checkout.js
@@ -1,9 +1,30 @@
 class Checkout {
 
-  total() {
-    return 0;
+  constructor(productRepository) {
+    this._productRepository = productRepository;
+    this._items = {};
   }
 
+  add(productId) {
+    return Promise.resolve(this._items[productId])
+      .then(item => item ? item : this._addItem(productId))
+      .then(item => item.qty += 1);
+  }
+
+  _addItem(productId) {
+    return this._productRepository.fetchById(productId).then(
+      product => {
+        this._items[productId] = {product, qty: 0};
+        return this._items[productId];
+      }
+    );
+  }
+
+  total() {
+    return Object.entries(this._items).reduce((sum, [_id, item]) => {
+        return sum + (item.qty * item.product.price);
+    }, 0);
+  }
 }
 
 module.exports = Checkout;
--- test/checkout-test.js
+++ test/checkout-test.js
@@ -7,4 +7,19 @@ describe('Checkout', () => {
     expect(checkout.total()).to.equal(0);
   });
 
+  it('calculates total as the sum of products when no price rules given', async function() {
+    const productStore = { fetchById: sinon.stub() };
+    productStore.fetchById.withArgs('A').returns(Promise.resolve({id: 'A', price: 100}));
+    productStore.fetchById.withArgs('B').returns(Promise.resolve({id: 'B', price: 200}));
+    productStore.fetchById.withArgs('C').returns(Promise.resolve({id: 'C', price: 300}));
+
+    const checkout = new Checkout(productStore);
+    await checkout.add('A');
+    await checkout.add('C');
+    await checkout.add('A');
+    
+    expect(checkout.total()).to.equal(500);
+  });
+
+
 });
